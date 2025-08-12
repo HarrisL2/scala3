0ce26eb8b584406da286a9eed0406c42d8dd15aa
HarrisL2 <harris.yh.lau@gmail.com>
2025-08-12 01:27:52 -0400

    WIP

--- library/src/scala/annotation/internal/ErasurePreservation.scala
+++ library/src/scala/annotation/internal/ErasurePreservation.scala
@@ -1,7 +1,12 @@
 package scala.annotation.internal
 
 import scala.annotation.Annotation
+import scala.annotation.meta.*
 
+@getter
+@setter
+@field
+@param
 class ErasurePreservation(path: String) extends Annotation {
 
 }
